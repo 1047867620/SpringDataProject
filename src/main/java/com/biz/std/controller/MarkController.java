ad49830e6ec650df6bb2120450c3a39f9155d3c2
yangming <yangming_1047867620@qq.com>
2017-12-18 23:47:01 +0800

    Initial commit

---
+++ src/main/java/com/biz/std/controller/MarkController.java
@@ -0,0 +1,19 @@
+package com.biz.std.controller;
+
+import com.biz.std.service.MarkService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class MarkController {
+    @Autowired
+    private MarkService markService;
+
+    @RequestMapping(value = "/addScore", method = RequestMethod.POST)
+    public String addScore(float[] scores, int[] subjectId, int studentId) {
+        markService.addScore(subjectId, scores, studentId);
+        return "redirect:/studentMain";
+    }
+}
